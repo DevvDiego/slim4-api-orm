b47fabd779fe921b8d5609647545e73afe44e7f2
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-02-28 20:24:22 -0600

    Change: Response helper to a Response trait to keep compatibility with classes

---
+++ src/Traits/ResponseTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Traits;
+
+use Psr\Http\Message\ResponseInterface as Response;
+
+trait ResponseTrait {
+
+    private function createResponse(
+        Response $response,
+        bool $success,
+        string $message,
+        int $statusCode,
+        ?array $data = null
+    ): Response {
+        
+        $responseData = [
+            "success" => $success,
+            "message" => $message,
+            "data" => $data
+        ];
+        
+        $response->getBody()->write(json_encode($responseData));
+        
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus($statusCode);
+    }
+
+    public function success(Response $res, $data = null, $msg = "success") {
+        return $this->createResponse($res, true, $msg, 200, $data);
+    }
+
+    public function created(Response $res, $data = null) {
+        return $this->createResponse($res, true, "Created successfully", 201, $data);
+    }
+
+    public function error(Response $res, $msg = "error", $code = 400) {
+        return $this->createResponse($res, false, $msg, $code);
+    }
+}
