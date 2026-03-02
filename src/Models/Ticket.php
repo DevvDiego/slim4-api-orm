9d53d2a1bd28cefbfe9daec257b9c4d3c37879c6
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-03-02 14:09:31 -0600

    Add Customer and Ticket models

--- src/Models/Ticket.php
+++ src/Models/Ticket.php
@@ -5,17 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model {
-    protected $table = "tickets";
-    protected $primaryKey = "ticket_id";
+
     protected $fillable = [
         "user_id", 
         "customer_id",
         "title",
         "value",
         "status"
-    ]; //enable massive fill of fields
-
-    public $timestamps = false; // use if sql has columns created_at/updated_at
+    ];
 
     public function user(){
         return $this->belongsTo(User::class, 'user_id', 'user_id');
