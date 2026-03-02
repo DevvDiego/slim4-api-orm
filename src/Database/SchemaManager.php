8587a95e6af33e052097a62966388ecd59763a3a
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-03-02 02:34:26 -0600

    Add SchemaManager to manage eloquent schemas for interacting with the columns of a database

---
+++ src/Database/SchemaManager.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Database;
+
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use App\Database\Schemas\UserSchema;
+use App\Traits\ResponseTrait;
+
+class SchemaManager {
+    use ResponseTrait;
+    protected $db;
+
+    public function __construct(Capsule $db) {
+        $this->db = $db;
+    }
+
+    public function sync(Request $request, Response $response, array $args): Response {
+        $this->db::schema()->disableForeignKeyConstraints();
+
+        try {
+            // create all tables first
+            UserSchema::create();
+            /* CustomerSchema::create();
+            TicketSchema::create(); */
+
+            // If there were any evolutions, update the tables then
+            $this->updateTables();
+
+            return $this->success($response, "Tables generated correctly");
+
+        } catch (\Exception $e) {
+            return $this->error($response, "Error creating tables: " . $e->getMessage(), 500);
+            
+        } finally {
+            //this always executes even after the return inside try or catch
+            $this->db::schema()->enableForeignKeyConstraints();
+        }
+    }
+
+    /**
+     * Any new fields must be added here to keep track of them more easily
+     */
+    private function updateTables() {
+        
+        /* $this->alter('users', 'phone', function($table) {
+            $table->string('phone', 20)->nullable();
+        });
+
+        $this->alter('customers', 'address', function($table) {
+            $table->text('address')->nullable();
+        }); */
+
+    }
+
+    /**
+     * Checks if table and row exists before applying other methods on it
+     */
+    private function alter(string $tableName, string $columnName, callable $callback): bool {
+
+        if ($this->db::schema()->hasTable($tableName) && 
+            !$this->db::schema()->hasColumn($tableName, $columnName)) 
+        {
+            // give control to the callback
+            $this->db::schema()->table($tableName, function ($table) use ($callback) {
+                $callback($table);
+            });
+            
+            return true;
+        }
+
+        return false;
+    }
+}
+
+?>
