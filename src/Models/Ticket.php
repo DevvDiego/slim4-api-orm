34759b6c3f96557ee1a3e0f3ff9b50097eb7d699
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-03-02 14:38:04 -0600

    Fix ticket relationship with user identifier

--- src/Models/Ticket.php
+++ src/Models/Ticket.php
@@ -15,7 +15,7 @@ class Ticket extends Model {
     ];
 
     public function user(){
-        return $this->belongsTo(User::class, 'user_id', 'user_id');
+        return $this->belongsTo(User::class, "user_id", "id");
     }
 
 }
