66c1fae52e672ac2a12027795f6e723d52cdc204
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-03-02 23:57:13 -0600

    Add simple validation middleware

---
+++ src/Middleware/ValidationMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Middleware;
+
+use App\Traits\ResponseTrait;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as Handler;
+use Slim\Psr7\Response;
+
+class ValidationMiddleware{
+    use ResponseTrait;
+    private array $requiredFields;
+
+    public function __construct(array $requiredFields){
+        $this->requiredFields = $requiredFields;
+    }
+
+    public function __invoke(Request $request, Handler $handler) {
+        $data = $request->getParsedBody() ?? [];
+        $errors = [];
+
+        foreach ($this->requiredFields as $field) {
+            if ( !isset($data[$field]) && empty($data[$field]) ){
+                $errors[] = "Field $field is required";
+            }
+        }
+
+        if ( !empty($errors) ) {
+
+            return $this->error(
+                new Response(),
+                $errors,
+                "Failed validation"
+            );
+
+        }
+
+        // if everything is ok, keep going with any other middleware
+        return $handler->handle($request);
+    }
+}
+
+
+?>
